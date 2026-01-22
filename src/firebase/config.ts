22d130c198e98b7313f4c9798ab490e33d0e39d5
Potti Jahnavi <jahnavi4627@gmail.com>
2026-01-22 13:17:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6731322600-685d3",
+  "appId": "1:164926542270:web:dd69723bbc44f53eb7e9ef",
+  "apiKey": "AIzaSyD5vrfAaourq-9zIPJYcaIVBVXFXGLek5s",
+  "authDomain": "studio-6731322600-685d3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "164926542270"
+};
